3a7aad96419989ac65d6856dc9a0aaee1861f403
Jitender <Jitenders2022@gmail.com>
2025-07-05 15:24:03 +0530

    Fix case-sensitive

--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import { Link, useNavigate } from "react-router-dom";
-import Input from "../../components/Inputs/Input";
+import Input from "../../components/Inputs/input";
 import { validateEmail } from "../../utils/helper";
 import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
 import axiosInstance from "../../utils/axiosInstance";
